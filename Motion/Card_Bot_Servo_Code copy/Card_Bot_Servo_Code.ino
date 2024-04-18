d521835087bdc7d40248a340383fd7e7925053bc
Liam-Battis <battis.l@northeastern.edu>
2024-04-17 20:58:55 -0400

    trial

--- Motion/Card_Bot_Servo_Code copy/Card_Bot_Servo_Code.ino
+++ Motion/Card_Bot_Servo_Code copy/Card_Bot_Servo_Code.ino
@@ -1,23 +1,14 @@
-#include <Servo.h>
-//#include <iostream>
-using namespace std;
-Servo myservo;
-int val;
-int pos = 0;
-void setup() {
-  // put your setup code here, to run once:
-myservo.attach(11);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-for (pos = 0; pos <= 45; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 45; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
+import time
+from adafruit_servokit import ServoKit
+myKit = ServoKit(channels = 16)
+myKit.servo[0].angle=180
+while True:
+    for i in range(0, 180):
+        myKit.servo[0].angle = i
+        time.sleep(.01)
+        print(i)
+    for i in range(180, 0, -1):
+        myKit.servo[0].angle = i
+        time.sleep(.01)
+        print(i)
+    break
